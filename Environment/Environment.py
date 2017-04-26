3f5a531cb284bcf93dd8fb70fd8aefbb7822e4d0
Zixun Ai <aiz@uci.edu>
2017-04-26 00:34:43 -0700

    Update Environment.py

--- Environment/Environment.py
+++ Environment/Environment.py
@@ -11,8 +11,6 @@ import time
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 
-# More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
-
 missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
             <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             
