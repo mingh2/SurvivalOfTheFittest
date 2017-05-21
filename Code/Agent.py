94b5ab47c1ac0d5cbb717a96ef23aca46074b5dc
mingh2 <mingh2@uci.edu>
2017-05-21 11:15:31 -0700

    Deep Q Network Implementation

--- Code/Agent.py
+++ Code/Agent.py
@@ -1,30 +1,176 @@
 import random
 import json
-from Action import action
+from Action import action as Move
+from NeuralNetwork import neural_network as nn
+from sys import maxint
+from collections import defaultdict, deque
 
 class zombies_fighter:
-    def __init__(self):
+    def __init__(self,  alpha=0.3, gamma=1, n=1):
+        """
+                Args
+            alpha:  <float>  learning rate      (default = 0.3)
+            gamma:  <float>  value decay rate   (default = 1)
+            n:      <int>    number of back steps to update (default = 1)
+        """
+
         self._counter = 0
+        self.epsilon = 0.20  # chance of taking a random action instead of the best
+
+        self.nn = nn(gamma)
+        self.n, self.gamma, self.alpha = n, alpha, gamma
+        self.previous_life = 0.0
+        self.previous_possable_actions = []
+
+        self.previous_state = None
+        self.previous_action = None
+        self.previous_reward = 0
+        self.previous_closest_distace = 0
+
+        self.action_map = {'left': 0, 'right': 1, 'up': 2, 'down': 3}
 
 
-    def act(self, agent_host, matrix, action):
+    def act(self, agent_host, action):
         """
         Args
-            Matrix:     NxN matrix, represent the environment 
-                        "clear":    No Wall, No Enemy 
-                        "blocked":  Wall, or other kind of unreachable location 
-                        "enemy":    Zombie or other kind of mob 
-            Action:     A class include all action available for the agent 
-                        up, down, left, right 
+            Matrix:     NxN matrix, represent the environment
+                        "clear":    No Wall, No Enemy
+                        "blocked":  Wall, or other kind of unreachable location
+                        "enemy":    Zombie or other kind of mob
+            Action:     A class include all action available for the agent
+                        up, down, left, right
         """
 
         self._counter += 1
 
-        print self._counter
-        if self._counter < 10:
-            action.up()
-        elif self._counter < 40:
-            action.attack()
-        else:
-            action.left()
+        move = Move(agent_host)
 
+        if action == 0:
+            move.left()
+            print 'left'
+        elif action == 1:
+            move.right()
+            print 'right'
+        elif action == 2:
+            move.up()
+            print 'up'
+        elif action == 3:
+            move.down()
+            print 'down'
+
+    def get_curr_state(self, agent_host, matrix):
+        wall = 0
+        enemy = 0
+
+        for col in range(len(matrix)):
+            for row in range(len(matrix[col])):
+                if matrix[col][row] == 'blocked':
+                    wall = wall + (col * 21 + row)
+                elif matrix[col][row] == 'enemy':
+                    enemy = col * 21 + row
+
+        # print "Wall: ", wall
+
+        while True:
+            world_state = agent_host.getWorldState()
+            if world_state.number_of_observations_since_last_state > 0:
+                msg = world_state.observations[-1].text
+                ob = json.loads(msg)
+                entities = ob[u'entities']
+
+                us = None
+                zombie = None
+
+                closeset_distance = maxint
+
+                for entity in entities:
+                    if entity[u'name'] == u'SOTF Bot':
+                        us = entity
+
+                for entity in entities:
+                    if entity[u'name'] == u'Zombie':
+                        zombie = entity
+
+                        distance = (us[u'x'] - zombie[u'x']) ** 2 + (us[u'z'] - zombie[u'z']) ** 2
+                        if distance < closeset_distance:
+                            closeset_distance = distance
+
+                return wall, enemy, ob[u'Life'], closeset_distance
+
+
+    def choose_action(self, curr_state, possible_actions):
+        rnd = random.random()
+
+        if rnd <= self.epsilon:
+            a = random.randint(0, len(possible_actions) - 1)
+            return possible_actions[a]
+
+        max_q_value = -maxint - 1
+        max_q_value_action = ''
+        for action in possible_actions:
+            q_value = self.nn.predict(curr_state + [action])
+            if q_value > max_q_value:
+                max_q_value = q_value
+                max_q_value_action = action
+        return max_q_value_action
+
+    def get_possible_actions(self, matrix):
+        possible_actions = []
+        if matrix[10][9] != 'blocked' and matrix[10][9] != 'enemy':
+            possible_actions.append(self.action_map['left'])
+        if matrix[10][11] != 'blocked' and matrix[10][11] != 'enemy':
+            possible_actions.append(self.action_map['right'])
+        if matrix[9][10] != 'blocked' and matrix[9][10] != 'enemy':
+            possible_actions.append(self.action_map['up'])
+        if matrix[11][10] != 'blocked' and matrix[11][10] != 'enemy':
+            possible_actions.append(self.action_map['down'])
+
+        return possible_actions
+
+    def replay(self, batch_size):
+        self.nn.replay(batch_size)
+
+    def run(self, agent_host, matrix):
+
+        wall, enemy, life, closeset_distance = self.get_curr_state(agent_host, matrix)
+
+        possible_actions = self.get_possible_actions(matrix)
+        a0 = self.choose_action([wall, enemy], possible_actions)
+
+        self.act(agent_host, a0)
+
+        if self.previous_state != None:
+            reward = 0
+
+            done = False
+            if life == 0.0:
+                done = True
+
+            if len(self.previous_possable_actions) > len(possible_actions):
+                reward = reward - 50
+            if self.previous_life > life:
+                reward = reward - 100
+
+            if self.previous_closest_distace > closeset_distance:
+                reward = reward - 25
+            elif self.previous_closest_distace < closeset_distance:
+                reward = reward + 25
+
+            if reward == 0:
+                reward = reward + 5
+
+            self.nn.remember(self.previous_state, self.previous_possable_actions,
+                self.previous_action, reward, [wall, enemy], done)
+
+            self.previous_reward = reward
+            self.previous_closest_distace = closeset_distance
+
+
+
+        self.previous_life = life
+        self.previous_possable_actions = possible_actions
+        self.previous_state = [wall, enemy]
+        self.previous_action = a0
+
+    def get_weights(self):
+        return self.nn.get_weights()
---
+++ Code/NeuralNetwork.py
@@ -0,0 +1,90 @@
+import random
+import numpy as np
+from sys import maxint
+
+random.seed(7)
+
+
+def sigmoid(x):
+    return 1.0/(1.0 + np.exp(-x))
+
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+class neural_network:
+    def __init__(self, gamma):
+        #Possible Moves: Correspond to Up, Down, Left, Right
+        self.layers = [3, 6, 1]
+        self.weights = []
+        self.memories = []
+
+        self.gamma = gamma
+
+        #range of weights (-1, 1)
+        for i in range (1, len(self.layers)-1):
+            wts = 2 * np.random.random((self.layers[i-1] + 1, self.layers[i] + 1)) - 1
+            self.weights.append(wts)
+
+        wts = 2 * np.random.random((self.layers[i] + 1, self.layers[i+1])) - 1
+        self.weights.append(wts)
+
+    def train(self, x, y, learning_rate = 0.2):
+
+        a = np.concatenate((np.ones(1).T, np.array(x)), axis=1)
+        a = np.reshape(a, (1, 4))
+
+        a = [a]
+
+        for i in range(len(self.weights)):
+            dot_value = np.dot(a[i], self.weights[i])
+            activation = sigmoid(dot_value)
+            a.append(activation)
+
+        error = y - a[-1]
+        deltas = [error * sigmoid_prime(a[-1])]
+
+        for i in range(len(a) - 2, 0, -1):
+            deltas.append(deltas[-1].dot(self.weights[i].T) * sigmoid_prime(a[i]))
+
+        deltas.reverse()
+        for i in range(len(self.weights)):
+            layer = np.atleast_2d(a[i])
+            delta = np.atleast_2d(deltas[i])
+            self.weights[i] += learning_rate * layer.T.dot(delta)
+
+    def predict(self, x):
+        a = np.concatenate((np.ones(1).T, np.array(x)), axis=1)
+        a = np.reshape(a, (1, 4))
+
+
+        for i in range(0, len(self.weights)):
+            a = sigmoid(np.dot(a, self.weights[i]))
+        return a
+
+    def remember(self, state, possible_actions, action, reward, next_state, done):
+        self.memories.append((state, possible_actions, action, reward, next_state, done))
+
+    def replay(self, batch_size):
+        print "LENGTH: ", len(self.memories)
+
+        batches = min(batch_size, len(self.memories))
+        batches = np.random.choice(len(self.memories), batches)
+
+        for i in batches:
+            state, possible_actions, action, reward, next_state, done = self.memories[i]
+            target = reward
+
+            if not done:
+                max_q_value = -maxint - 1
+                max_q_value_action = ''
+                for action in possible_actions:
+                    q_value = self.predict(state + [action])
+                    if q_value > max_q_value:
+                        max_q_value = q_value
+                        max_q_value_action = action
+                target = target + self.gamma * max_q_value
+
+            self.train(state + [action], target)
+
+    def get_weights(self):
+        return self.weights
